b39c977f958b01d0f65c98afc3595de52b3c3646
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2019-01-01 18:42:23 +0100

    add queries and mutations

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+
+// Resolvers
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+// Resolvers
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => ({ ...request, prisma })
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
---
+++ src/components/CreateLink.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+import { FEED_QUERY } from "./LinkList";
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+class CreateLink extends Component {
+  state = {
+    description: "",
+    url: ""
+  };
+
+  render() {
+    const { description, url } = this.state;
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            type="text"
+            className="mb2"
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
+            placeholder="A description for the link"
+          />
+          <input
+            type="text"
+            className="mb2"
+            value={url}
+            onChange={e => this.setState({ url: e.target.value })}
+            placeholder="A url for the link"
+          />
+        </div>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push("/")}
+          update={(store, { data: { post } }) => {
+            const data = store.readQuery({ query: FEED_QUERY });
+            data.feed.links.unshift(post);
+            store.writeQuery({ query: FEED_QUERY, data });
+          }}
+        >
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    );
+  }
+}
+
+export default CreateLink;
